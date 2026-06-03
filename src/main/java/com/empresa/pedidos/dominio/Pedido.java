650da6db3f56f6c8254d7cd60e814d784bbf25fc
Alessandro Reyes <alessandrorl@ufps.edu.co>
2026-06-02 21:04:59 -0500

    test: violacion de arquitectura intencional

--- src/main/java/com/empresa/pedidos/dominio/Pedido.java
+++ src/main/java/com/empresa/pedidos/dominio/Pedido.java
@@ -59,4 +59,11 @@ public class Pedido {
     public void setEstado(EstadoPedido estado) {
         this.estado = estado;
     }
+
+    // Metodo introducido intencionalmente para provocar una dependencia hacia infraestructura
+    @SuppressWarnings("unused")
+    public void introduceInfraDependency() {
+        com.empresa.pedidos.infraestructura.persistencia.PedidoEntity e = new com.empresa.pedidos.infraestructura.persistencia.PedidoEntity();
+        e.setId(this.id);
+    }
 }
